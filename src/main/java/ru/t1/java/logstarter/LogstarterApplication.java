089510485e0fac5781824636d638e1a062473e1d
Nikvitya <nikvitya@yandex.ru>
2025-02-12 16:51:34 +0300

    Initial commit

---
+++ src/main/java/ru/t1/java/logstarter/LogstarterApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.java.logstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/t1/java/logstarter/LogstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.java.logstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
